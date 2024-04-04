705b2e13ad3cd0b3097983e9493682e0c3c2e5e1
Nathaniel Quansah <n.bongo40@gmail.com>
2024-04-04 13:47:00 +0000

    added events and tests

--- lib/git_smart_web/controllers/live/page_live/repo/repo.ex
+++ lib/git_smart_web/controllers/live/page_live/repo/repo.ex
@@ -1,3 +1,15 @@
 defmodule GitSmartWeb.PageLive.Repo do
   use GitSmartWeb, :live_component
+
+  def update(assigns, socket) do
+    socket = socket |> assign(assigns) |> assign(is_added: false)
+
+    {:ok, socket}
+  end
+
+  def handle_event("add_repo", _params, socket) do
+      socket = socket |> assign(is_added: true)
+
+    {:noreply, socket}
+  end
 end
--- test/git_smart_web/controllers/live/page_live/repo_test.exs
+++ test/git_smart_web/controllers/live/page_live/repo_test.exs
@@ -13,4 +13,16 @@ defmodule GitSmartWeb.PageLive.RepoTest do
       assert has_element?(lv, "li#elixir>div>div>button")
     end
   end
+
+  describe "handle_event/3 - add repo" do
+    test "handle the event to add repo", %{conn: conn} do
+      {:ok, lv, _html} = live(conn, ~p"/")
+
+      refute has_element?(lv, "li#elixir>div>div", "Repo added!")
+
+      lv |> element("button[phx-click=add_repo]") |> render_click()
+
+      assert has_element?(lv, "li#elixir>div>div", "Repo added!")
+    end
+  end
 end
