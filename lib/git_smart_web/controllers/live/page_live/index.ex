193f2a8f32b8c73b6f9b79e48f872d56a50fdeac
Nathaniel Quansah <n.bongo40@gmail.com>
2024-04-04 18:49:46 +0000

    finalized lab -1

--- lib/git_smart_web/controllers/live/page_live/index.ex
+++ lib/git_smart_web/controllers/live/page_live/index.ex
@@ -12,12 +12,32 @@ defmodule GitSmartWeb.PageLive.Index do
     page = params["page"] || 1
     repositories = Repositories.list(language, page)
 
+    socket =
+      socket
+      |> assign(language: language)
+      |> assign(page: page)
+      |> update_repositories(repositories)
+
+    {:noreply, socket}
+  end
+
+  def handle_event("load_more", _params, socket) do
+    language = socket.assigns.language
+    page = socket.assigns.page + 1
+    repositories = Repositories.list(language, page)
+
+    socket = update_repositories(socket, repositories) |> assign(page: page)
+
+    {:noreply, socket}
+  end
+
+  defp update_repositories(socket, repositories) do
     case repositories do
       {:error, message} ->
-        {:noreply, socket |> put_flash(:error, message) |> assign(:repositories, [])}
+        socket |> put_flash(:error, message) |> stream(:repositories, [])
 
       repositories ->
-        {:noreply, assign(socket, :repositories, repositories)}
+        stream(socket, :repositories, repositories)
     end
   end
 end
